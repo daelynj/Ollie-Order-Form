24ab1324f3189fef22ecac7b66b96a4569fd6ef3
daelynj <daelyndbjones@gmail.com>
2020-09-27 13:51:32 -0700

    Add update logic for products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -50,20 +50,35 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+    @order.line_items.destroy_all
+
+    order_params[:line_items].each do |item|
+      if !item.empty?
+        @order.line_items.create(:product_id => item, :order_id => @order.id)
       end
+    end
+
+    respond_to do |format|
+      format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+      format.json { render :show, status: :ok, location: @order }
     end
   end
 
   def submit
+    if (@order.line_items.length == 0)
+      respond_to do |format|
+        format.html { redirect_to orders_path, notice: 'Cannot submit empty order.' }
+        format.json { render :show, status: :ok, location: orders_path }
+      end
+      return
+    end
+
     @order.update(status: true)
-    redirect_to orders_path
+
+    respond_to do |format|
+      format.html { redirect_to orders_path, notice: 'Order was successfully updated.' }
+      format.json { render :show, status: :ok, location: orders_path }
+    end
   end
 
   private
